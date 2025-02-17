f4237097423ce26fa8fde9e9b2280ff0efee846b
Hamnah137 <149074750+Hamnah137@users.noreply.github.com>
2025-02-17 10:36:58 -0800

    right file

--- db_1.sql
+++ db_1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 13, 2025 at 03:44 PM
+-- Generation Time: Feb 17, 2025 at 02:00 PM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.18
 
@@ -23,49 +23,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` text,
-  `price` decimal(10,2) NOT NULL,
-  `image_path` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `shop` varchar(255) NOT NULL DEFAULT 'main'
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `profile_image` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `products`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `products` (`product_id`, `name`, `description`, `price`, `image_path`, `created_at`, `shop`) VALUES
-(1, 'RED JUMPER WITH A SWAN', 'Keep you warm in this winter season ;)', '19.99', 'https://aidashoreditch.co.uk/cdn/shop/files/EWC409_1_600x.jpg?v=1727880838', '2025-02-05 11:17:16', 'main'),
-(2, 'GREEN HOODIE', 'Twin with nature, twin with us :)', '29.99', 'https://aidashoreditch.co.uk/cdn/shop/products/CMC379_GREEN_FLAT_600x.jpg?v=1664882952', '2025-02-05 11:19:24', 'main'),
-(3, 'BLUE DENIM JACKET', 'Go with fashion with us', '39.99', 'https://aidashoreditch.co.uk/cdn/shop/files/EWC365_5_600x.jpg?v=1726246127', '2025-02-05 11:21:30', 'main'),
-(6, 'Children Overhead Hoodie', 'Go with the fashion:)', '15.00', 'https://assets.ajio.com/medias/sys_master/root/20221101/6Qig/63602143f997ddfdbd4e21ae/-473Wx593H-441589790-blue-MODEL.jpg', '2025-02-13 15:25:52', 'main'),
-(7, 'Children Overhead Hoodie', 'Go with the fashion:)', '15.00', 'https://www.tradeprint.co.uk/dam/jcr:e64f5c2d-8100-4508-80d9-b24d519b477f/sun-yellow.webp', '2025-02-13 15:25:52', 'main');
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `profile_image`, `is_admin`, `created_at`) VALUES
+(1, 'linajohn', 'dollyyy', 'linajohn@rrr.com', NULL, 0, '2025-01-29 11:08:31'),
+(2, 'evanbravo', 'password', 'evan123@gmail.com', NULL, 0, '2025-01-29 11:26:10'),
+(3, 'marvi12', 'raven', 'marvi123@gmail.www', '', 0, '2025-02-05 09:28:07'),
+(4, 'minakhan', 'hello', 'minakhan@yahoo.com', '', 0, '2025-02-12 11:49:10');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `products`
+-- Indexes for table `users`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
